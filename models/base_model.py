406e066c880ce5082bf841ea5fe70fb901c6160f
juansechaux <juansechaux@hotmail.com>
2023-11-02 17:04:46 -0500

    airBnB task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,18 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at":
+                    self.created_at = value
+                elif key == "updated_at":
+                    self.updated_at = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         self.updated_at = datetime.now()
