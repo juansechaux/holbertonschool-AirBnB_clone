8e317183a694e33d3e223f6760c1d9eeff97175a
juansechaux <juansechaux@hotmail.com>
2023-11-03 08:58:32 -0500

    Task 5

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+
+print('Primer paso initialization')
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 '''This is the module for BaseModel Class'''
 
-
+print('Cuarto paso initialization')
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if len(kwargs) > 0:
@@ -18,9 +19,11 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def __str__(self):
         """Magic method str,
